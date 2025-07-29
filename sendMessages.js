1e9fa1cc504db1b2ed8e32c62768c45adbf80468
kobato-chan1912 <no_body156@yahoo.com.vn>
2025-07-29 22:12:30 +0700

    update

--- sendMessages.js
+++ sendMessages.js
@@ -73,120 +73,121 @@ export async function processPendingMessages(instances) {
                             phone: null, uid: userId, zaloname: zaloName
 
                         }
-
-
-                        globalSendID = alias.uid;
-                        globalSendName = alias.zaloname;
-
-
-                        // Gửi nội dung
-
-
-                        if (content) {
-                            await api.sendMessage(content, alias.uid, ThreadType.User);
-
-                        }
-
-                        // Gửi file
-                        if (attachment_path) {
-                            await api.sendMessage(
-                                {
-                                    msg: '',
-                                    attachments: [path.resolve(attachment_path)],
-                                },
-                                alias.uid
-                            );
-                        }
                     }
+
+
+                    globalSendID = alias.uid;
+                    globalSendName = alias.zaloname;
+
+
+                    // Gửi nội dung
+
+
+                    if (content) {
+                        await api.sendMessage(content, alias.uid, ThreadType.User);
+
+                    }
+
+                    // Gửi file
+                    if (attachment_path) {
+                        await api.sendMessage(
+                            {
+                                msg: '',
+                                attachments: [path.resolve(attachment_path)],
+                            },
+                            alias.uid
+                        );
+                    }
+                }
 
                 // ==== Group ====
                 else if (send_type === 'group') {
-                        let groupUid = null;
+                    let groupUid = null;
 
-                        const isNumberReceiver = isNaturalNumber(zalo_receiver);
-                        if (!isNumberReceiver) {
-                            const allGroups = await api.getAllGroups();
-                            const groupIds = Object.keys(allGroups.gridVerMap);
+                    const isNumberReceiver = isNaturalNumber(zalo_receiver);
+                    if (!isNumberReceiver) {
+                        const allGroups = await api.getAllGroups();
+                        const groupIds = Object.keys(allGroups.gridVerMap);
 
-                            for (const gid of groupIds) {
-                                const groupInfo = await api.getGroupInfo(gid);
-                                const group = groupInfo.gridInfoMap?.[gid];
-                                if (group?.name === zalo_receiver) {
-                                    groupUid = gid;
-                                    globalSendID = gid;
-                                    globalSendName = group.name;
-                                    break;
-                                }
+                        for (const gid of groupIds) {
+                            const groupInfo = await api.getGroupInfo(gid);
+                            const group = groupInfo.gridInfoMap?.[gid];
+                            if (group?.name === zalo_receiver) {
+                                groupUid = gid;
+                                globalSendID = gid;
+                                globalSendName = group.name;
+                                break;
                             }
-
-
-                            if (!groupUid) throw new Error('Không tìm thấy group phù hợp');
-
-                        } else {
-                            groupUid = zalo_receiver;
-                            globalSendID = zalo_receiver;
-                            const groupInfo = await api.getGroupInfo(groupUid);
-                            const group = groupInfo.gridInfoMap?.[groupUid];
-                            globalSendName = group.name;
-
-                            if (!globalSendName) throw new Error('Không tìm thấy group phù hợp');
                         }
 
-                        if (content) {
-                            await api.sendMessage(content, groupUid, ThreadType.Group);
-                        }
 
-                        if (attachment_path) {
-                            await api.sendMessage(
-                                {
-                                    msg: '',
-                                    attachments: [path.resolve(attachment_path)],
-                                },
-                                groupUid,
-                                ThreadType.Group
-                            );
-                        }
+                        if (!groupUid) throw new Error('Không tìm thấy group phù hợp');
+
+                    } else {
+                        groupUid = zalo_receiver;
+                        globalSendID = zalo_receiver;
+                        const groupInfo = await api.getGroupInfo(groupUid);
+                        const group = groupInfo.gridInfoMap?.[groupUid];
+                        globalSendName = group.name;
+
+                        if (!globalSendName) throw new Error('Không tìm thấy group phù hợp');
                     }
 
-                    // ✅ Cập nhật trạng thái đã gửi
-                    await callReturnStatusSendMessage(msg.id, 'done');
-                    console.log(`✅ Gửi tin nhắn ID ${id} thành công`);
-                    let imagePath = null;
-                    let filePath = null;
+                    if (content) {
+                        await api.sendMessage(content, groupUid, ThreadType.Group);
+                    }
+
                     if (attachment_path) {
-                        if (attachment_path.endsWith('.jpg') || attachment_path.endsWith('.png')
-                            || attachment_path.endsWith('.jpeg') || attachment_path.endsWith('.gif')) {
-                            imagePath = path.resolve(attachment_path);
-                        } else {
-                            filePath = path.resolve(attachment_path);
-                        }
+                        await api.sendMessage(
+                            {
+                                msg: '',
+                                attachments: [path.resolve(attachment_path)],
+                            },
+                            groupUid,
+                            ThreadType.Group
+                        );
                     }
-
-                    let myInfo = await api.fetchAccountInfo()
-                    const myUserId = myInfo?.profile?.userId || null;
-                    const myZaloName = myInfo?.profile?.zaloName || null;
-
-                    const replyData = ({
-                        message_id: "self-sent",
-                        sendTo: globalSendName,
-                        sendTo_id: globalSendID,
-                        sender: myZaloName,
-                        sender_id: myUserId,
-                        zalo_receiver: accountData.myUserId,
-                        text: content,
-                        image: imagePath,
-                        file: filePath
-                    });
-
-                    await callReplyZaloMessages(replyData);
-                    //
-
-                } catch (err) {
-                    console.log(err)
-                    console.error(`❌ Gửi tin nhắn ID ${id} lỗi:`, err.message);
-                    await callReturnStatusSendMessage(msg.id, 'error', err.message);
                 }
+
+                // ✅ Cập nhật trạng thái đã gửi
+                await callReturnStatusSendMessage(msg.id, 'done');
+                console.log(`✅ Gửi tin nhắn ID ${id} thành công`);
+                let imagePath = null;
+                let filePath = null;
+                if (attachment_path) {
+                    if (attachment_path.endsWith('.jpg') || attachment_path.endsWith('.png')
+                        || attachment_path.endsWith('.jpeg') || attachment_path.endsWith('.gif')) {
+                        imagePath = path.resolve(attachment_path);
+                    } else {
+                        filePath = path.resolve(attachment_path);
+                    }
+                }
+
+                let myInfo = await api.fetchAccountInfo()
+                const myUserId = myInfo?.profile?.userId || null;
+                const myZaloName = myInfo?.profile?.zaloName || null;
+
+                const replyData = ({
+                    message_id: "self-sent",
+                    sendTo: globalSendName,
+                    sendTo_id: globalSendID,
+                    sender: myZaloName,
+                    sender_id: myUserId,
+                    zalo_receiver: accountData.myUserId,
+                    text: content,
+                    image: imagePath,
+                    file: filePath
+                });
+
+                await callReplyZaloMessages(replyData);
+                //
+
+            } catch (err) {
+                console.log(err)
+                console.error(`❌ Gửi tin nhắn ID ${id} lỗi:`, err.message);
+                await callReturnStatusSendMessage(msg.id, 'error', err.message);
             }
+        }
 
     }
-    }
+}
